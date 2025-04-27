c1cab880e0c5a0b6e769234c876e6211559ee5c9
unknown <mdshahnewazs77@gmail.com>
2025-04-28 00:23:49 +0600

    update

--- routes/frontend.php
+++ routes/frontend.php
@@ -6,31 +6,6 @@ use App\Http\Controllers\Frontend\PagesController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-Route::get('/cache-clear', function () {
-    Artisan::call('cache:clear');
-    Artisan::call('route:clear');
-    Artisan::call('config:clear');
-    Artisan::call('view:clear');
-    Artisan::call('clear-compiled');
-    Artisan::call('optimize:clear');
-    Artisan::call('storage:link');
-    // Artisan::call('optimize');
-    session()->flash('message', 'System Updated Successfully.');
-    return redirect()->route('frontend.home');
-});
-
 Route::get('/', [PagesController::class, 'home'])->name('home');
 Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('cart.add');
 Route::post('/cart/remove', [CartController::class, 'removeFromCart'])->name('cart.remove');
